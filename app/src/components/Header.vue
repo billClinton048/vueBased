c340943c7d5b951b22a9947345f59e34ac9c6f46
billClinton048 <clintonkaranja46@gmail.com>
2024-06-01 03:35:42 +0300

    message

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -2,17 +2,17 @@
   <div
     class="container my-10 flex max-w-7xl mx-auto justify-between items-center"
   >
-    <div class=""><h1 class="text-orange-700 text-3xl ml-5">Logo</h1></div>
+    <div class=""><h1 class="text-orange-700 text-3xl cursor-pointer ml-5 hover:text-orange-600 transition">Logo</h1></div>
     <div class="nav ">
-      <ul class=" max-w-2xl flex justify-between items-center">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contacts</a></li>
-        <li><a href="#">Services</a></li>
+      <ul class=" max-w-2xl flex gap-10  justify-between items-center">
+        <li><a href="#" class="hover:text-orange-700 font-bold transition">Home</a></li>
+        <li><a href="#" class="hover:text-orange-700 font-bold transition">About</a></li>
+        <li><a href="#" class="hover:text-orange-700 font-bold transition">Contacts</a></li>
+        <li><a href="#" class="hover:text-orange-700 font-bold transition">Services</a></li>
       </ul>
     </div>
     <div class="btn ">
-      <button class="text-lg rounded-3xl bg-orange-700 text-white px-5 ">Get Started</button>
+      <button class="text-lg rounded-3xl bg-orange-700 text-white px-5 hover:bg-orange-500 transition">Get Started</button>
     </div>
   </div>
 </template>
