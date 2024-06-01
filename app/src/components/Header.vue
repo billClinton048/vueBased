70b5dd263e680e8cdecdd325c5566e613d3e29ac
billClinton048 <clintonkaranja46@gmail.com>
2024-06-01 09:23:05 +0300

    message

--- app/src/components/Header.vue
+++ app/src/components/Header.vue
@@ -2,17 +2,23 @@
   <div
     class="container my-10 flex max-w-7xl mx-auto justify-between items-center"
   >
-    <div class=""><h1 class="text-orange-700 text-3xl ml-5">Logo</h1></div>
+    <div class=""><h1 class="text-orange-700 text-xl ml-5">Logo</h1></div>
     <div class="nav ">
-      <ul class=" max-w-2xl flex justify-between items-center">
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contacts</a></li>
-        <li><a href="#">Services</a></li>
+      <ul class=" max-w-2xl flex justify-between  gap-20 items-center">
+        <li><a href="#" class="text-orange-700 uppercase transition-all hover:text-orange-500" >Home</a></li>
+        <li><a href="#" class="text-orange-700 uppercase transition-all hover:text-orange-500" >About</a></li>
+        <li><a href="#" class="text-orange-700 uppercase transition-all hover:text-orange-500" >Contacts</a></li>
+        <li><a href="#" class="text-orange-700 uppercase transition-all hover:text-orange-500" >Services</a></li>
       </ul>
     </div>
+  
     <div class="btn ">
-      <button class="text-lg rounded-3xl bg-orange-700 text-white px-5 ">Get Started</button>
+      <button class="text-lg rounded-3xl bg-orange-700 text-white px-5 hover:bg-orange-500 ">Get Started</button>
+    </div>
+    <div class="w-10 h-5 flex flex-col justify-between items-center sm:hidden">
+      <div class="w-7 h-1 bg-orange-700"></div>
+      <div class="w-7 h-1 bg-orange-700"></div>
+      <div class="w-7 h-1 bg-orange-700"></div>
     </div>
   </div>
 </template>
