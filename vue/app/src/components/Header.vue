19acc2113162ad7698c79598f9e470d8f477bba2
billClinton048 <162782447+billClinton048@users.noreply.github.com>
2024-09-03 23:58:46 +0300

    Add files via upload

---
+++ vue/app/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Header hello </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ vue/app/src/router/index.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { HomeView,} from "../views";
+
+
+const routes = [
+  { path: "/", name: "home", component: HomeView },
+ 
+];
+
+/**Initialize here */
+const router = createRouter({ history: createWebHistory(), routes });
+
+
+export default router;
